d17aae0749cddff2d2fa125834f6201bd854f3ba
clemesakerele22 <akereleclement@gmail.com>
2023-03-08 17:38:09 -0800

    4. Once an idea has taken hold of the brain it's almost impossible to eradicate

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _pow_recursion - search a string for any of set of bytes.
+ * _pow_recursion - Search a string for any of a set of bytes.
  * @x: base
  * @y: exposant
- * Retun: Pointer to the byte in 's' that matches one of the bytes in 'accept'
+ * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
  * or NULL if no such byte is found.
  */
 
