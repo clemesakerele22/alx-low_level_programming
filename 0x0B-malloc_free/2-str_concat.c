84011a0c4fe928df2cb8ffa7eecd8614efc2e6b7
clemesakerele22 <akereleclement@gmail.com>
2023-03-15 02:23:05 -0700

    2. He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,49 +1,32 @@
+#include "main.h"
 #include <stdlib.h>
-#include "holberton.h"
-
 /**
- * *str_concat - concatenates two strings
- * @s1: string to concatenate
- * @s2: other string to concatenate
- *
- * Return: pointer to the new string created (Success), or NULL (Error)
+ * str_concat - concatenates the trings s1 & s2
+ * @s1: string 1
+ * @s2: string 2
+ * Return: NULL (Failure) or Pointer address
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *s3;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	int size1 = 0, size2 = 0, i = 0, j = 0, sum;
+	char *ptr;
 
-	while (s1 && s1[len1])
-		len1++;
-	while (s2 && s2[len2])
-		len2++;
-
-	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (s3 == NULL)
+	if (s1 == NULL)
+		size1 = 0;
+	if (s2 == NULL)
+		size2 = 0;
+	for (; s1[size1]; size1++)
+		;
+	for (; s2[size2]; size2++)
+		;
+	sum = size1 + size2;
+	ptr = malloc(sizeof(char) * sum + 1);
+	if (ptr == NULL)
 		return (NULL);
-
-	i = 0;
-	j = 0;
-
-	if (s1)
-	{
-		while (i < len1)
-		{
-			s3[i] = s1[i];
-			i++;
-		}
-	}
-
-	if (s2)
-	{
-		while (i < (len1 + len2))
-		{
-			s3[i] = s2[j];
-			i++;
-			j++;
-		}
-	}
-	s3[i] = '\0';
-
-	return (s3);
+	for (; i < size1; i++)
+		ptr[i] = s1[i];
+	for (; j < size2; j++)
+		ptr[j + size1] = s2[j];
+	ptr[sum] = '\0';
+	return (ptr);
 }
